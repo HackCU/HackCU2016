2052977bab82885e2dac01e99a0b08b805dd1a8e
albarianjess <albarian.jess@gmail.com>
2015-11-24 09:54:35 -0700

    Added awesome star animation

---
+++ js/stars.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var limit=100, // Max number of starts
+container=document.getElementById('test');
+loop={
+	//initilizing
+	start:function(){
+		for (var i=0; i <= limit; i++) {
+			var star=this.newStar();
+			star.style.top=this.rand()*100+"%";
+			star.style.left=this.rand()*100+"%";
+			star.style.webkitAnimationDelay=this.rand()+"s";
+			star.style.mozAnimationDelay=this.rand()+"s";
+			container.appendChild(star);
+		};
+	},
+	//to get random number
+	rand:function(){
+		return Math.random();
+	},
+	//createing html dom for star
+	newStar:function(){
+		var d = document.createElement('div');
+		d.innerHTML = '<figure class="star"><figure class="star-top"></figure><figure class="star-bottom"></figure></figure>';
+ 		return d.firstChild;
+	},
+};
+loop.start();
+
+
+
+
