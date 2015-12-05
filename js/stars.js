55b6722bd0e5d8bb6530c9a6007ad95b9e5ffb14
albarianjess <albarian.jess@gmail.com>
2015-12-05 11:14:14 -0700

    Fixed. The. Stars. FInally.

--- js/stars.js
+++ js/stars.js
@@ -1,33 +1,37 @@
 
 
-var limit=70, // Max number of stars
-container = document.getElementById('star');
-loop={
+var limit=60, // Max number of stars
+    loop = {
 	//initilizing
-	start:function(){
-		for (var i=0; i <= limit; i++) {
+	start:function(starDOM) {
+		for (var i=0; i < limit; i++) {
 			var star=this.newStar();
 			star.style.top=this.rand()*100+"%";
 			star.style.left=this.rand()*100+"%";
 			star.style.webkitAnimationDelay=this.rand()+"s";
 			star.style.mozAnimationDelay=this.rand()+"s";
-			container.appendChild(star);
-		};
+			if (starDOM) {
+                            starDOM.appendChild(star);
+                        }
+                };
 	},
 	//to get random number
-	rand:function(){
+	rand:function() {
 		return Math.random();
 	},
         
 	//creating html for star
-	newStar:function(){
+	newStar:function() {
 		var d = document.createElement('div');
 		d.innerHTML = '<figure class="star"><figure class="star-top"></figure><figure class="star-bottom"></figure></figure>';
  		return d.firstChild;
 	}
-        
 };
-loop.start();
+
+stars = document.getElementsByClassName('stars');
+for(var i = 0; i < stars.length; i++) {
+    loop.start(stars[i]);
+}
 
 
 
