8f2e8f1b30a051a0d52db9356f66db5e0593aa2e
dawsonbotsford <dawsonbotsford@gmail.com>
2016-01-29 14:56:16 -0700

    grunt copy coplete and workding

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,24 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    clean: ["dist"]
+    clean: ["dist"],
+		copy: {
+      copy_untouched_resources_to_dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['**/*'],
+            dest: 'dist'
+          }
+        ]
+      }
+    }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('production', []);
   grunt.registerTask('default', []);
