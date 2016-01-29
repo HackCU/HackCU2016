d712077ba6ada7124b9c40b44815d6d3789bfcdc
dawsonbotsford <dawsonbotsford@gmail.com>
2016-01-29 15:28:39 -0700

    * grunt imagemin * grunt newer

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,29 @@ module.exports = function(grunt) {
           {
             expand: true,
             cwd: 'src',
-            src: ['**/*'],
+            src: ['**/*', '!**/*.{png,jpg,gif}'],
             dest: 'dist'
           }
         ]
       }
+    },
+		imagemin: {                          // Task
+			dynamic: {
+				files: [{
+					expand: true,                  // Enable dynamic expansion
+					cwd: 'src/',                   // Src matches are relative to this path
+					src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+					dest: 'dist/'                  // Destination path prefix
+				}]
+			}
     }
-
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-newer');
 
-  grunt.registerTask('production', []);
-  grunt.registerTask('default', []);
+  grunt.registerTask('production', ['clean', 'copy', 'imagemin']);
+  grunt.registerTask('default', ['newer:copy', 'newer:imagemin']);
 };
--- README.md
+++ README.md
@@ -5,12 +5,17 @@
 ## Installation
 Install node and npm
 
+First time contributor:
 ```shell
 npm install -g grunt-cli
+```
 
-
+Continuing Contributors:
+```shell
+npm install
 
 ```
+
 When adding node modules: `npm install --save <node_module_name>
 ```
 
