03d5a978b5892146a2b4346489f8f75c5cc8bb21
jessalbarian <albarian.jess@gmail.com>
2016-03-10 17:28:19 -0700

    added css/js from another template for better nav and extra features I want to add

--- src/js/stars.js
+++ src/js/stars.js
@@ -1,39 +1,15 @@
-
-
-var limit = 50, // Max number of stars
-    loop = {
-	//initilizing
-	start:function(starDOM) {
-		for (var i=0; i < limit; i++) {
-			var star=this.newStar();
-			star.style.top=this.rand()*100+"%";
-			star.style.left=this.rand()*100+"%";
-			star.style.webkitAnimationDelay=this.rand()+"s";
-			star.style.mozAnimationDelay=this.rand()+"s";
-			if (starDOM) {
-                            starDOM.appendChild(star);
-                        }
-                }
-	},
-	//to get random number
-	rand:function() {
-		return Math.random();
-	},
-        
-	//creating html for stars
-	newStar:function() {
-		var d = document.createElement('div');
-		d.innerHTML = '<figure class="star"><figure class="star-top"></figure><figure class="star-bottom"></figure></figure>';
- 		return d.firstChild;
-	}
+var limit = 30, loop = {
+    start: function (a) {
+        for (var b = 0; limit > b; b++) {
+            var c = this.newStar();
+            c.style.top = 100 * this.rand() + "%", c.style.left = 100 * this.rand() + "%", c.style.webkitAnimationDelay = this.rand() + "s", c.style.mozAnimationDelay = this.rand() + "s", a && a.appendChild(c)
+        }
+    }, rand: function () {
+        return Math.random()
+    }, newStar: function () {
+        var a = document.createElement("div");
+        return a.innerHTML = '<figure class="star"><figure class="star-top"></figure><figure class="star-bottom"></figure></figure>', a.firstChild
+    }
 };
-
-//loop through to place stars at divs containing 'stars'
-stars = document.getElementsByClassName('stars');
-for(var i = 0; i < 100; i++) {
-    loop.start(stars[i]);
-}
-
-
-
-
+stars = document.getElementsByClassName("stars");
+for (var i = 0; 100 > i; i++)loop.start(stars[i]);
